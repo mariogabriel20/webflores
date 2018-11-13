1997fd3f5d379a840e92f10ef1749ce6523becc4
Mario <gabbriel.mario@gmail.com>
2018-11-13 17:15:12 -0300

    catalogo
    cambie el catalogo

--- taller/resources/views/catalogo.blade.php
+++ taller/resources/views/catalogo.blade.php
@@ -3,74 +3,125 @@
 @section('contenido')
 <div class="card container mx-auto">
 <div class="container mt-5">
-	<div class="card-group">
-  		<div class="card text-white bg-info mb-3">
+  <div class="card-group">
+      <div class="card color">
         <div class="card-body">
-    		<img class="card-img-top" src="../img/cards/image1.jpg" class="img-fluid" alt="Card image cap">
-    		<div class="card-body">
-      		<h5 class="card-title">Florsaca blancaca</h5>
-      		<p class="card-text">Alta florsaca blancaca.</p>
-    		</div>
-    		<div class="card-footer">
-      		<small class="text-muted">Last updated 3 mins ago</small>
-    		</div>
-  		</div>
+        <img class="card-img-top" src="img/cards/image6.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Ramo de flores</h5>
+          <p class="card-text">Elegante ramo de flores.</p>
+        </div>
+        <br>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
     </div>
-  		<div class="card">
-    		<img class="card-img-top" src="http://www.floresdeocoa.cl/wp-content/uploads/2018/01/Ramo-home.png" class="img-fluid" alt="Card image cap">
-    		<div class="card-body">
-      		<h5 class="card-title">Card title</h5>
-      		<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    		</div>
-    		<div class="card-footer">
-      		<small class="text-muted">Last updated 3 mins ago</small>
-    		</div>
-  		</div>
-  		<div class="card text-white bg-info mb-3">
-    		<img class="card-img-top" src="http://www.florespedia.com/Imagenes/flores-bonitas.jpg" class="img-fluid" alt="Card image cap">
-    		<div class="card-body">
-     		<h5 class="card-title">Card title</h5>
-      		<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    		</div>
-    		<div class="card-footer">
-      		<small class="text-muted">Last updated 3 mins ago</small>
-    		</div>
-  		</div>
-	</div>
-	<div class="card-group">
-  		<div class="card">
-    		<img class="card-img-top" src="http://www.florespedia.com/Imagenes/flores-bonitas.jpg" class="img-fluid" alt="Card image cap">
-    		<div class="card-body">
-      		<h5 class="card-title">Card title</h5>
-      		<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    		</div>
-    		<div class="card-footer">
-      		<small class="text-muted">Last updated 3 mins ago</small>
-    		</div>
-  		</div>
-  		<div class="card">
-    		<img class="card-img-top" src="https://www.lysaflores.com/wp-content/uploads/2017/11/Ramo-Pink-lady-a-domicilio.jpg" class="img-fluid" alt="Card image cap">
-    		<div class="card-body">
-      		<h5 class="card-title">Card title</h5>
-      		<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    		</div>
-    		<div class="card-footer">
-      		<small class="text-muted">Last updated 3 mins ago</small>
-    		</div>
-  		</div>
-  		<div class="card">
-    		<img class="card-img-top" src="images/cards/image2.jpg" class="img-fluid" alt="Card image cap">
-    		<div class="card-body">
-      		<h5 class="card-title">Card title</h5>
-      		<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    		</div>
-    		<div class="card-footer">
-      		<small class="text-muted">Last updated 3 mins ago</small>
-    		</div>
-  		</div>
-	</div>
+      <div class="card color">
+        <div class="card-body">
+        <img class="card-img-top" src="img/cards/image8.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Flores blancas</h5>
+          <p class="card-text">Flores adornadas en botellas transparentes.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      </div>
+      <div class="card color">
+        <div class="card-body">
+        <img class="card-img-top" src="img/cards/image9.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Muchas flores</h5>
+          <p class="card-text">Flores hermosas.</p>
+        </div>
+        <br>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card-group">
+      <div class="card color">
+        <div class="card-body">
+        <img class="card-img-top" src="img/cards/image4.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Flores</h5>
+          <p class="card-text">Flores en bolsitas.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      </div>
+     <div class="card color">
+        <div class="card-body">
+        <img class="card-img-top" src="img/cards/image5.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Flores</h5>
+          <p class="card-text">Flores en bolsitas.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      </div>
+      <div class="card color">
+        <div class="card-body">
+        <img class="card-img-top" src="img/cards/image7.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Flores</h5>
+          <p class="card-text">Flores en bolsitas.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      </div>
+  </div>
+  <div class="card-group">
+      <div class="card color">
+        <div class="card-body">
+        <img class="card-img-top" src="img/cards/image13.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Flores</h5>
+          <p class="card-text">Flores en bolsitas.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      </div>
+     <div class="card color">
+        <div class="card-body">
+        <img class="card-img-top" src="img/cards/image12.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Flores</h5>
+          <p class="card-text">Flores en bolsitas.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      </div>
+      <div class="card color">
+        <div class="card-body">
+        <img class="card-img-top" src="img/cards/image11.jpg" class="img-fluid" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Flores</h5>
+          <p class="card-text">Flores en bolsitas.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      </div>
+  </div>
 </div>
-
+<br>
+<br>
 </div>
 
 
